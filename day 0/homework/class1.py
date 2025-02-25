b0cb7c3b014cb4bf5cdf27521eb8ad29782b98f7
Niako2013 <baiadze97@bk.ru>
2025-02-25 20:34:38 +0100

    first commit

---
+++ day 0/homework/class1.py
@@ -0,0 +1,12 @@
+#turtle
+from turtle import*
+forward(200)
+color("red")
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+color("red")
+exitonclick()
---
+++ day 01/homework/level02.py
@@ -0,0 +1,5 @@
+name="NIA"
+lastname="UPLISASHVILI"
+age=11
+height=1.50
+print(name, lastname, age, height)
