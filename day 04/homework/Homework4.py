b1cc9d03637c7a7edde401ad77c8d769ad2118a0
Niako2013 <baiadze97@bk.ru>
2025-03-01 09:45:46 +0100

    commit

---
+++ day 04/homework/Homework4.py
@@ -0,0 +1,6 @@
+math1=6578+9095
+math2=859/4
+math3=6509*70
+math4=87-20
+sum= math1+math2+math3+math4
+print(sum)
---
+++ day 05/classwork.py
@@ -0,0 +1,27 @@
+# კომენტარებით ახსენი რა არის case-sensitive და snake-case, შემდეგ ორივეზე დაწერე 3 მაგალითი, ასევე გაასწორეთ ეს კოდები:
+name="Nika"
+print(Name)
+
+goa best = 100
+print(goa best)
+
+goa best=100 
+print(Goa best)
+
+name - 'mate'
+print(naMe)
+
+name="Mate"
+print(Name)
+
+number=10
+print(Number)
+
+number = 10
+print{number)
+
+goabest=100%
+print(Goabest)
+
+goabest = "100%)
+print(goaBest)
